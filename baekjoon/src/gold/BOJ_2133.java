9e142317f0612326f53bf0e4c72cd212bf7b25ae
= <skylove0911@naver.com>
2022-10-04 23:42:37 +0900

    solved

--- baekjoon/src/gold/BOJ_2133.java
+++ baekjoon/src/gold/BOJ_2133.java
@@ -8,7 +8,19 @@ public class BOJ_2133 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] dp = new int[N+1];
-		dp[2] = 3;
+		int[] dp = new int[N + 1];
+		if (N >= 2) {
+			dp[0] = 1;
+			dp[2] = 3;
+			for (int i = 4; i <= N; i += 2) {
+				dp[i] += 3 * dp[i - 2];
+				for (int j = 4; j <= i; j += 2) {
+					dp[i] += 2 * dp[i - j];
+				}
+			}
+		}
+
+		System.out.println(dp[N]);
+
 	}
 }
